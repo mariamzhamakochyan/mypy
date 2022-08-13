91deb86ebe911064f116fc47ca240111381de8c8
mariamzhamakochyan <107346258+mariamzhamakochyan@users.noreply.github.com>
2022-08-13 16:24:06 +0400

    Create sub_of_digits.py

---
+++ sub_of_digits.py
@@ -0,0 +1,10 @@
+def difference(num):
+    sum = 0
+    multiple = 1
+    while n != 0
+          sum += n % 10
+          multiple *= n % 10
+          n = n // 10
+    return multiple - sum
+n = 4637
+print(difference(num))
