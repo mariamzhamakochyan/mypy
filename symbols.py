293d9822f9300ba85ecc576e7083fca01074e227
mariamzhamakochyan <107346258+mariamzhamakochyan@users.noreply.github.com>
2022-08-15 15:38:59 +0400

    Update symbols.py

--- symbols.py
+++ symbols.py
@@ -1,6 +1,8 @@
 count = 0
 filename = input("Enter your filename :")
 file = open(filename, 'r')
-for symbols in file:
-    count += len(symbols)
+file = file.read()
+file = file.split()
+for words in file:
+    count += len(words)
 print(count)
